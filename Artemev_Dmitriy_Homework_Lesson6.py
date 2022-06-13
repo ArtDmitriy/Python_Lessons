ff324ab9ce8711d29cc34c059da43e5982db4191
Dmitriy <artemev_dmitriy_1997@mail.ru>
2022-06-13 16:08:51 +0300

    Lesson6

---
+++ Artemev_Dmitriy_Homework_Lesson6.py
@@ -0,0 +1,126 @@
+# Урок 6. Работа с файлами.
+
+
+# Задание №1. Не используя библиотеки для парсинга, распарсить (получить определённые данные) файл логов web-сервера nginx_logs.txt
+# (https://github.com/elastic/examples/raw/master/Common%20Data%20Formats/nginx_logs/nginx_logs) — получить список кортежей вида:
+# (<remote_addr>, <request_type>, <requested_resource>).
+# Например:
+# [
+# ...
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('141.138.90.60', 'GET', '/downloads/product_2'),
+# ('173.255.199.22', 'GET', '/downloads/product_2'),
+# ...
+# ]
+# Примечание: код должен работать даже с файлами, размер которых превышает объем ОЗУ компьютера.
+#
+
+#
+# def parse_log(pth_file):
+#
+#     if pth_file:
+#         with open(pth_file, "r", encoding="utf-8") as file:
+#             for line in file:
+#                 ip = line.split(" - - ")[0]
+#                 rsp_and_pth = line.split('"')[1]
+#                 rsp = rsp_and_pth.split()[0]
+#                 pth = rsp_and_pth.split()[1]
+#                 yield(ip, rsp, pth)
+#
+#
+# if __name__ == "__main__":
+#     a = parse_log("./nginx_logs.txt")
+#     for e in a:
+#         print(e)
+
+
+# Задание №3. Есть два файла: в одном хранятся ФИО пользователей сайта, а в другом — данные об их хобби.
+# Известно, что при хранении данных используется принцип: одна строка — один пользователь,
+# разделитель между значениями — запятая. Написать код, загружающий данные из обоих файлов и формирующий из них словарь:
+# ключи — ФИО, значения — данные о хобби. Сохранить словарь в файл. Проверить сохранённые данные.
+# Если в файле, хранящем данные о хобби, меньше записей, чем в файле с ФИО, задаём в словаре значение None.
+# Если наоборот — выходим из скрипта с кодом «1».
+# При решении задачи считать, что объём данных в файлах во много раз меньше объема ОЗУ.
+
+# Фрагмент файла с данными о пользователях (users.csv):
+# Иванов,Иван,Иванович
+# Петров,Петр,Петрович
+# Фрагмент файла с данными о хобби (hobby.csv):
+# скалолазание,охота
+# горные лыжи
+
+import os
+import json
+
+from itertools import zip_longest
+
+
+def groping(
+        user_pth="./users.csv",
+        hobby_pth="./hobby.csv",
+        output_pth="./out.txt"):
+
+    # test of exists files
+    if not (os.path.isfile(user_pth) or
+            os.path.isfile(hobby_pth)):
+        return -1
+
+    user_lines = None
+    hobby_lines = None
+
+    with open(user_pth, "r", encoding="utf-8") as user_file:
+        user_lines = user_file.readlines()
+
+    with open(hobby_pth, "r", encoding="utf-8") as hobby_file:
+        hobby_lines = hobby_file.readlines()
+
+    if len(user_lines) < len(hobby_lines):
+        return 1
+
+    group = {}
+
+    for fio, hobby in zip_longest(user_lines, hobby_lines):
+        fio = fio.replace("\n", "")
+        group[fio] = hobby.replace("\n", "") if hobby else None
+
+    with open(output_pth, "w+", encoding="utf-8") as out_file:
+        json.dump(group, out_file)  # out_file.writelines(json.dumps(group))
+
+    return 0
+
+
+if __name__ == "__main__":
+
+    exit(groping())
+
+# Задание №6. Реализовать простую систему хранения данных о суммах продаж булочной.
+# Должно быть два скрипта с интерфейсом командной строки: для записи данных и для вывода на экран записанных данных.
+# При записи передавать из командной строки значение суммы продаж.
+# Для чтения данных реализовать в командной строке следующую логику:
+# просто запуск скрипта — выводить все записи;
+# запуск скрипта с одним параметром-числом — выводить все записи с номера, равного этому числу, до конца;
+# запуск скрипта с двумя числами — выводить записи, начиная с номера, равного первому числу, по номер,
+# равный второму числу, включительно.
+# Подумать, как избежать чтения всего файла при реализации второго и третьего случаев.
+# Данные хранить в файле bakery.csv в кодировке utf-8. Нумерация записей начинается с 1.
+# Примеры запуска скриптов:
+# python add_sale.py 5978,5
+# python add_sale.py 8914,3
+# python add_sale.py 7879,1
+# python add_sale.py 1573,7
+# python show_sales.py
+# 5978,5
+# 8914,3
+# 7879,1
+# 1573,7
+# python show_sales.py 3
+# 7879,1
+# 1573,7
+# python show_sales.py 1 3
+# 5978,5
+# 8914,3
+# 7879,1
+
+
+
+
